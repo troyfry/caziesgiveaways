2bca62279bec2b880adc58a933f86dc2183dd98f
troyfry <67286327+troyfry@users.noreply.github.com>
2025-01-22 20:36:32 -0500

    Update prompting-cheat.py

--- prompting-cheat.py
+++ prompting-cheat.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Jotform Submission", page_icon="📋", layout="centered")
 
 # App Title
-st.title("Free Prompting Sheat Sheet")
+st.title("Free Prompting Cheat Sheet")
 
 # Description
 st.write("""
